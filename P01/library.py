ea811005de557c23fda2db110e12f42e05f4e6f1
Michael Kirby <kirbyma729@gmail.com>
2018-02-04 16:02:20 -0500

    project 1 folder

---
+++ P01/library.py
@@ -0,0 +1,50 @@
+#library file of shared functions
+import sys
+import string
+
+
+def caesar(text, key):
+    for i in text:
+        if i.isalpha():
+            i = i.lower()
+            shifted = chr( ord(i) + key)
+            if shifted.isalpha() == 0:
+                shifted = chr( ord(shifted) - 26)
+            sys.stdout.write(shifted)
+        else:
+            sys.stdout.write(i)
+
+
+def removeDuplicates(text):   #remove duplicate characters in a string
+    seen = set()
+    unique = []
+    for char in text:
+        if char not in seen:
+            seen.add(char)
+            unique.append(char)
+
+    return "".join(unique)     #string of unique characters
+
+
+def chunkText(text):   #chunk string of text into blocks of 2 with a space in between each chunk
+    chunked = ""
+    for c in range(0, len(text), 2):
+        chunked = chunked + text[c:c+2] + " "
+
+    return chunked
+
+
+def replaceCharacters(text, in_table, out_table):    #replace chars from 'in_table' with chars from 'out_table'
+    table = string.maketrans(in_table, out_table)
+    return text.translate(table)
+
+
+def findLetter(dictionary, row, column):     #find a key in a dictionary where the associated value is a tuple of numbers
+    for k, v in dictionary.iteritems():    #iterate over key-value pairs to find key with specific value we searched for
+        if v == (row, column):              #'k' stands for key of dictionary, 'v' is a value associated with a key in a dictionary
+            return k
+
+
+def onlyAlphabetChars(text):
+    text = filter(lambda c: c.isalpha(), text)
+    return text
---
+++ P01/mono-encrypt.py
@@ -0,0 +1,30 @@
+#Monoalphabetic encryption scheme
+
+import sys
+import monoalphabetic
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) == 2:
+        
+        key = sys.argv[1]
+        input_text = sys.stdin.read()   #standard input
+        
+        #print "key: %s \ninput text: %s" % (key, input_text)
+        
+    elif len(sys.argv) == 3:
+        
+        key = sys.argv[1]
+        
+        with open(sys.argv[2], 'r') as textfile:  #read in text file
+            input_text = textfile.read()
+            
+        #print "key: %s \ninput text: %s" % (key, input_text)
+        
+    else:
+        print "Error: Incorrect arguments given."
+        sys.exit(1)
+        
+        
+    ciphertext = monoalphabetic.encrypt(key, input_text)
+    sys.stdout.write(ciphertext)
