3ddaf30dd0fb6b0a83d36b1653ca6937c8487bce
Michael Kirby <kirbyma729@gmail.com>
2018-03-21 18:43:11 -0400

    project 2

---
+++ P02/md5-iden-1byte.cpp
@@ -0,0 +1,26 @@
+/* https://www.openssl.org/docs/manmaster/man3/MD5_Update.html
+   
+   Compile with: g++ -O3 md5.cpp -Wall -lssl  -lcrypto
+*/
+#include <openssl/md5.h>
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    unsigned char output[16] = {};  // 128bits is 16 bytes
+    // MD5 outputs a 128bit message digest, which is 16 bytes long
+    
+    for(int i = 0; i < 256; i++){
+        char in = i;
+        MD5((unsigned char*)&in, 1, output);
+        if(in == output[0]){
+            cout << "Input: " << i << ", is the same as output: " << i << endl;
+        }
+    }
+    cout << endl;
+}
---
+++ P02/md5-iden-3byte.cpp
@@ -0,0 +1,27 @@
+/* https://www.openssl.org/docs/manmaster/man3/MD5_Update.html
+   
+   Compile with: g++ -O3 md5.cpp -Wall -lssl  -lcrypto
+*/
+#include <openssl/md5.h>
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+
+    unsigned char output[16] = {};  // 128bits is 16 bytes
+    // MD5 outputs a 128bit message digest, which is 16 bytes long
+
+    for(int i = 0; i < 256 * 256 * 256; i++){
+        int in = i;
+        MD5((unsigned char*)&in, 3, output);
+        if(memcmp(&in, output, 3) == 0 ){
+            cout << "Input: " << i << ", is the same as output: " << i << endl;
+        }
+    }
+    cout << endl;
+}
